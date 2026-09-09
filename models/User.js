fb41ffebf9f087f48c3d5a2c5d80babc8fed6fbc
victoricoma <victorsena3010@gmail.com>
2026-09-09 12:14:16 -0300

    Model Update Aula0909

--- models/User.js
+++ models/User.js
@@ -1,12 +1,16 @@
 export class User {
-    constructor({ uid, email, displayName, photoURL, emailVerified, phoneNumber, createdAt, updatedAt }) {
-        this.uid = uid;
-        this.email = email;
-        this.displayName = displayName || '';
-        this.photoURL = photoURL || '';
-        this.emailVerified = emailVerified || false;
-        this.phoneNumber = phoneNumber || '';
-        this.createdAt = createdAt || new Date();
-        this.updatedAt = updatedAt || new Date();
+    constructor({ uid, email, password, displayName, 
+        photoURL, emailVerified, phoneNumber, 
+        disable, createdAt, updatedAt }) {
+        this.uid = uid
+        this.email = email
+        this.displayName = displayName || ''
+        this.photoURL = photoURL || ''
+        this.password = password
+        this.emailVerified = emailVerified || false
+        this.phoneNumber = phoneNumber || ''
+        this.disable = disable || false
+        this.createdAt = createdAt || new Date()
+        this.updatedAt = updatedAt || new Date()
     }
 }
